8c8d9df629d8b5f5a6070eac1e73204ab7cf4ce1
liuzihan1234 <1654985534@qq.com>
2020-12-15 19:45:24 +0800

    初始化

---
+++ day01_Git/src/com/itheima/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+        System.out.println("HelloGit");
+        System.out.println("Dev∑÷÷ß");
+        System.out.println("111");
+    }
+}
